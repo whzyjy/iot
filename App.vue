1de1d5437f4778527efa8ebe452e342d084fb9c0
whzyjy <129816028+whzyjy@users.noreply.github.com>
2023-05-24 15:33:58 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Navbar></Navbar>
+    <v-contenr>
+      <router-view>
+
+      </router-view>
+    </v-contenr>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+
+export default {
+    name: "App",
+    components : {
+      Navbar
+    },
+    data: () => ({
+    //
+    }),
+
+};
+</script>
